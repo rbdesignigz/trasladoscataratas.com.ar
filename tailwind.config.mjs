714bfad5a9e6da03b82c8a3118a49926303d80e6
rbdesignigz <rbdesignigz@gmail.com>
2025-09-20 23:00:31 -0300

    primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#211f16',      // Fondo oscuro, texto principal
+        secondary: '#fbfbeb',    // Fondo claro, cards
+        accent: '#d99e27',       // Botones, destacados
+        highlight: '#c6af7f',    // Hover, bordes
+        darkAccent: '#866a32',   // Botones secundarios
+        lightAccent: '#ede6ba',  // Hover cards
+        slateBlue: '#a3b3bd',    // Texto secundario
+        blueGray: '#cad6df',     // Fondo alternativo
+        beige: '#bcb4ac',        // Fondo cards alternativo
+        tealGray: '#7494a4',     // Links, botones secundarios
+      },
+    },
+  },
+}
